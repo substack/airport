a8bfc1f481dcd811ea9e19c6393cfc0fab3e50a3
James Halliday <mail@substack.net>
2012-03-26 13:52:20 -0700

    setInterval in the connect example to better demonstrate what happens when nodes come up and down

--- example/connect/connect.js
+++ example/connect/connect.js
@@ -5,8 +5,10 @@ var ports = seaport.connect('localhost', 9090);
 var air = airport(ports);
 
 var up = air.connect('beep');
-up(function (remote) {
-    remote.fives(11, function (n) {
-        console.log('fives(11) : ' + n);
+setInterval(function () {
+    up(function (remote) {
+        remote.fives(11, function (n) {
+            console.log('fives(11) : ' + n);
+        });
     });
-});
+}, 1000);
